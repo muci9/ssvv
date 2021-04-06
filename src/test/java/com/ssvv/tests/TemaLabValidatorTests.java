8e972fca53e46ab83531652fc2ecdffcec28bc1e
Ciprian-Adrian Muresan <muresan.ciprian99@gmail.com>
2021-04-06 08:23:36 +0300

    test add assignment

--- src/test/java/com/ssvv/tests/TemaLabValidatorTests.java
+++ src/test/java/com/ssvv/tests/TemaLabValidatorTests.java
@@ -2,7 +2,10 @@ package com.ssvv.tests;
 
 import com.ssvv.Domain.TemaLab;
 import com.ssvv.Exceptions.ValidatorException;
+import com.ssvv.Repository.XMLFileRepository.TemaLabXMLRepo;
+import com.ssvv.Service.XMLFileService.TemaLabXMLService;
 import com.ssvv.Validator.TemaLabValidator;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,6 +14,9 @@ import org.junit.rules.ExpectedException;
 public class TemaLabValidatorTests {
 
     private TemaLabValidator temaLabValidator;
+    private TemaLabXMLRepo temaLabXMLRepo;
+    private TemaLabXMLService temaLabXMLService;
+    private String filename = "tema-lab-tests.xml";
 
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
@@ -18,23 +24,77 @@ public class TemaLabValidatorTests {
     @Before
     public void setup() {
         temaLabValidator = new TemaLabValidator();
+        temaLabXMLRepo = new TemaLabXMLRepo(temaLabValidator, filename);
+        temaLabXMLService = new TemaLabXMLService(temaLabXMLRepo);
     }
 
     @Test
-    public void testValidateAssignmentWithInvalidDueDate() throws ValidatorException {
+    public void testAddAssignmentTC1() throws ValidatorException {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Entity can not be null!\n");
+
+        temaLabXMLRepo.save(null);
+    }
+
+    @Test
+    public void testAddAssignmentTC2() throws ValidatorException {
         exceptionRule.expect(ValidatorException.class);
-        exceptionRule.expectMessage("Termen limita invalid\n");
+        exceptionRule.expectMessage("Nr tema invalid\n");
 
-        TemaLab temaLabFailDueDate = new TemaLab(1, "Test validator", 15, 13);
-        temaLabValidator.validate(temaLabFailDueDate);
+        TemaLab temaLabFailID = new TemaLab(null, "Test", 12, 10);
+        temaLabXMLRepo.save(temaLabFailID);
     }
 
     @Test
-    public void testValidateAssignmentWithInvalidDescription() throws ValidatorException {
+    public void testAddAssignmentTC3() throws ValidatorException {
         exceptionRule.expect(ValidatorException.class);
         exceptionRule.expectMessage("Descriere tema invalida\n");
 
-        TemaLab temaLabFailDescription = new TemaLab(1, "", 12, 10);
-        temaLabValidator.validate(temaLabFailDescription);
+        String[] params = new String[] {"1", "", "14", "1"};
+        temaLabXMLService.add(params);
+    }
+
+    @Test
+    public void testAddAssignmentTC4_1() throws ValidatorException {
+        exceptionRule.expect(ValidatorException.class);
+        exceptionRule.expectMessage("Sapatamana predarii invalida\n");
+
+        String[] params = new String[] {"1", "Test", "1", "0"};
+        temaLabXMLService.add(params);
+    }
+
+//    @Test
+//    public void testAddAssignmentTC4() throws ValidatorException {
+//        exceptionRule.expect(ValidatorException.class);
+//        exceptionRule.expectMessage("Sapatamana predarii invalida\n");
+//
+//        String[] params = new String[] {"1", "Test", "1", "15"};
+//        temaLabXMLService.add(params);
+//    }
+
+    @Test
+    public void testAddAssignmentTC5() throws ValidatorException {
+        exceptionRule.expect(ValidatorException.class);
+        exceptionRule.expectMessage("Termen limita invalid\n");
+
+        String[] params = new String[] {"1", "Test", "0", "1"};
+        temaLabXMLService.add(params);
+    }
+
+    @Test
+    public void testAddAssignmentTC6() throws ValidatorException {
+        String[] params = new String[] {"1", "Test", "2", "1"};
+
+        temaLabXMLService.add(params);
+        Assert.assertNotNull(temaLabXMLService.findOne(1));
+    }
+
+    @Test
+    public void testAddAssignmentTC7() throws ValidatorException {
+        String[] params1 = new String[] {"1", "Test 1", "2", "1"};
+
+        temaLabXMLService.add(params1);
+        temaLabXMLService.add(params1);
+        Assert.assertNotNull(temaLabXMLService.findOne(1));
     }
 }
